86ee33014874716abccd7df3675611593ba61b1a
Brandon Bernal <bernal.brandon@gmail.com>
2020-05-26 19:04:43 -0400

    Updates

---
+++ precache-manifest.72a4c1defe7e5fc8a7d71cac5bb4d1a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96efb88b25bb4a02f8f25e8933781ad4",
+    "url": "/nimble-markdown/index.html"
+  },
+  {
+    "revision": "4f37db23fc8101bfc2b4",
+    "url": "/nimble-markdown/static/css/main.85ad7960.chunk.css"
+  },
+  {
+    "revision": "1a36ce4ac224a9c734e7",
+    "url": "/nimble-markdown/static/js/2.59da2533.chunk.js"
+  },
+  {
+    "revision": "714872843b8176ecc38433f1fc418b5b",
+    "url": "/nimble-markdown/static/js/2.59da2533.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f37db23fc8101bfc2b4",
+    "url": "/nimble-markdown/static/js/main.67d27ba7.chunk.js"
+  },
+  {
+    "revision": "e82d0f688fcc5086fc4a",
+    "url": "/nimble-markdown/static/js/runtime-main.b96a2499.js"
+  }
+]);
