814ff3de09ce66365f8b8db312afc51e0391ea8e
Brandon Bernal <bernal.brandon@gmail.com>
2020-05-27 17:21:27 -0400

    Add fading navbar on scroll and smooth scroll on arrow click

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import nimblelogo from './nimble-logo.png';
 import AceEditor from "react-ace";
 import MarkdownView from "react-showdown";
@@ -15,16 +15,30 @@ import "ace-builds/src-noconflict/theme-textmate";
 import "ace-builds/src-noconflict/theme-solarized_dark";
 import "ace-builds/src-noconflict/theme-solarized_light";
 import "ace-builds/src-noconflict/theme-terminal";
+import { Link } from 'react-scroll';
 
 function onChange(newValue) {
   console.log("change", newValue);
 }
+
+// function useOnScreen(options) {
+//   const ref = useRef();
+//   const [visivle, setVisible] = useState(false);
+
+//   useEffect(() => {
+//     const heroObserver = new InteractionObserver(([entry]) => {
+//       setVisible(entry.isIntersecting);
+//     }, options);
+//     if (ref.current) heroObserver.observe(ref.current);
+//   }, [ref, options]);
+// }
 
 function App() {
   let [markdown, setMarkDown] = useState(`### Hello! :wave:\n\nThanks for visiting **nimble** :rabbit:
   \n\nEdit me and get a live preview of the markdown output thanks to the power of React! :sunglasses:
   \n\nTry changing the theme of the editor too! :art:`);
   let [theme, setTheme] = useState('monokai');
+  // const ref = { current: null };
 
   const themeOptions = themes.map((theme, i) => (
     <option className="theme-option" key={theme}>
@@ -36,18 +50,52 @@ function App() {
     setTheme(selectedTheme);
   }
 
+  useEffect(() => {
+    const navbar = document.querySelector('.navbar');
+    const hero = document.getElementById('hero');
+    const themeSelector = document.querySelector('.theme-selector');
+
+    const heroOptions = {
+      rootMargin: '-600px 0px 0px 0px'
+    };
+
+    const heroObserver = new IntersectionObserver((entries, heroObserver) => {
+      entries.forEach((entry) => {
+        console.log(entry.target);
+        if (!entry.isIntersecting) { // User has scrolled
+          navbar.classList.add('scrolled');
+          themeSelector.classList.remove('navtop');
+          navbar.classList.remove('navtop');
+        } else { // Navbar is at the top of the page
+          navbar.classList.add('navtop');
+          themeSelector.classList.add('navtop');
+          navbar.classList.remove('scrolled');
+        }
+      })
+    }, heroOptions);
+
+    heroObserver.observe(hero);
+  }, []);
+
   return (
     <div className="App">
-      <div className='page'>
+      <div className='page' id='hero'> 
         <div>
           <h1 className='landing-page-header'>nimble</h1>
           <h2>A simple markdown editor</h2>
         </div>
-        <i class="fas fa-arrow-down"></i>
+        <Link 
+          to='editor' 
+          smooth={true} 
+          duration={500}
+        >
+          <i className="fas fa-arrow-down" />
+        </Link>
       </div>
       <nav className='navbar navbar-expand-lg navbar-light fixed-top'>
         <div className="theme-selector">
-          <label><i class="fas fa-paint-roller"></i></label>
+          <label><i className="fas fa-paint-roller"></i></label>
+          {/* <Link to='/#editor'><i className="fas fa-paint-roller"></i></Link> */}
           <select
             name="theme-names"
             id="theme-names"
@@ -68,6 +116,7 @@ function App() {
           <div className="theme-selector-and-editor">
             <AceEditor
               className="editor"
+              id="editor"
               mode="markdown"
               theme={theme}
               onChange={(value, stat) => {
